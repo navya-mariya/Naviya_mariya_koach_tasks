467859c693659be300fbaa465dd1744d07419831
navya-mariya <71212457+navya-mariya@users.noreply.github.com>
2021-12-18 21:23:08 +0530

    level 5

---
+++ client.php
@@ -0,0 +1,12 @@
+<?php
+$host    = "127.0.0.1";
+$port    = 25003;
+$message = "Hello Server";
+echo "Message To server :".$message;
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+$result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
+socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+$result = socket_read ($socket, 1024) or die("Could not read server response\n");
+echo "Reply From Server  :".$result;
+socket_close($socket);
+?>
